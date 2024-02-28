ecb4ea3419f4453fcce5a50dd92ce9abf64d0da4
vic-sv <56349392+vic-sv@users.noreply.github.com>
2024-02-28 20:08:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from matplotlib import pyplot as plt
+import numpy as np
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Here'
+    a = np.array([1, 2, 3, 4])
+    m = np.mean(a)
+    return f"Hello from Here - {m}"
 
 
 if __name__ == "__main__":
